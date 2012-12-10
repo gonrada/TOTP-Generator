79bff157e24523b4044cb64ea0d8a1a5cc56f852
gonrada <gonrada@gmail.com>
2012-12-09 19:50:38 -0500

    Now using temios to disable echo when password is entered

--- generator.h
+++ generator.h
@@ -44,7 +44,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <termios.h>
 #include <time.h>
+#include <unistd.h>
 
 #ifdef DEBUG
 #define PRINTD( format, ... ) fprintf(stderr,format, __VA_ARGS__ );
